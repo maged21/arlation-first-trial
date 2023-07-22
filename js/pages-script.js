a7a0051e33906069f944066a982745b3da496098
Maged Aziz <majedabdulaziz72@yahoo.com>
2023-07-22 22:45:08 +0300

    last update

--- js/pages-script.js
+++ js/pages-script.js
@@ -10,3 +10,80 @@ span.onclick = function () {
         behavior: "smooth",
     })
 }
+
+
+// ---------------- second navbar
+
+const tl = gsap.timeline({ paused: true });
+
+const animateOpenNav = () => {
+    tl.to("#navtwo-container", {
+        duration: 0.2,
+        autoAlpha: 1,
+        delay: 0.1,
+    });
+
+    tl.to(".site-logo", {
+        duration: 0.2,
+        color: "#fff"
+    },
+    "-=0.1"
+    );
+};
+
+const openNav = () => {
+    animateOpenNav();
+    const navBtn = document.getElementById("menu-toggle-btn");
+    navBtn.onclick = function (e) {
+        navBtn.classList.toggle("active");
+        tl.reversed(!tl.reversed());
+
+    };
+};
+
+openNav();
+
+tl.from(".nvtwo > div", {
+    duration: 0.4,
+    opacity: 0,
+    y: 10,
+    stagger: {
+        amount: 0.04,
+    },
+});
+
+tl.to(".navtwo-link > a", {
+    duration: 0.8,
+    top: 0,
+    ease: "power2.inOut",
+    stagger: {
+        amount: 0.1
+    },
+},
+"-=0.4"
+);
+
+tl.from(".nav-footer", {
+    duration: 0.3,
+    opacity: 0
+}, "-=0.5").reverse();
+
+
+// Wait for the document to be fully loaded
+let prevScrollPos = 0;
+
+function toggleSecondNavbar() {
+  const scrollPosition = scrollContainer.scrollTop;
+  const secondNavbar = document.querySelector(".navbartwo");
+
+  if (scrollPosition === 0) {
+    secondNavbar.classList.remove("show");
+  } else if (scrollPosition > prevScrollPos) {
+    secondNavbar.classList.remove("show");
+  } else {
+    secondNavbar.classList.add("show");
+  }
+
+  prevScrollPos = scrollPosition;
+}
+
--- js/script.js
+++ js/script.js
@@ -13,41 +13,26 @@ TweenMax.staggerFrom(".navbar-nav .nav-item", 1,{
   ease: Power3.easeInOut
 }, 0.12)
 
-
-TweenMax.from('.banner_text h1', 1, {
-  delay:0.8,
+TweenMax.from(".head-text", 1,{
+  delay:1.3,
   opacity:0,
-  x: -20,
-  ease: Power3.easeInOut
+  y: -20,
+  ease: Power4.easeIn
 })
 
-TweenMax.from(".banner_text p", 1,{
-  delay:1.2,
+TweenMax.from(".head-p", 1,{
+  delay:1.5,
   opacity:0,
-  x: 20,
-  ease: Power3.easeInOut
+  y: 20,
+  ease: Power4.easeIn
 })
 
 TweenMax.from(".btn_group", 1,{
-  delay:.8,
+  delay:2,
   opacity:0,
-  y: -20,
+  y: 20,
   ease: Power3.easeInOut
 })
-
-TweenMax.from(".banner_img", 1,{
-  delay:2.4,
-  opacity:0,
-  x: 600,
-  ease: Power4.easeInOut
-})
-
-TweenMax.staggerFrom(".block_wrap .block", 1,{
-  delay:1.5,
-  opacity:0,
-  x: -30,
-  ease: Power4.easeInOut
-}, 0.08)
 
 
 
@@ -76,6 +61,7 @@ const openNav = () => {
     navBtn.onclick = function (e) {
         navBtn.classList.toggle("active");
         tl.reversed(!tl.reversed());
+
     };
 };
 
@@ -108,21 +94,192 @@ tl.from(".nav-footer", {
 
 
 // Wait for the document to be fully loaded
-document.addEventListener("DOMContentLoaded", function() {
-    var secondNavbar = document.querySelector(".navbartwo");
-  
-    // Function to toggle the visibility of the second navbar
-    function toggleSecondNavbar() {
-      if (window.scrollY > 310) {
-        secondNavbar.classList.add("show");
-      } else {
-        secondNavbar.classList.remove("show");
-      }
+let prevScrollPos = 0;
+
+function toggleSecondNavbar() {
+  const scrollPosition = scrollContainer.scrollTop;
+  const secondNavbar = document.querySelector(".navbartwo");
+
+  if (scrollPosition === 0) {
+    secondNavbar.classList.remove("show");
+  } else if (scrollPosition > prevScrollPos) {
+    secondNavbar.classList.remove("show");
+  } else {
+    secondNavbar.classList.add("show");
+  }
+
+  prevScrollPos = scrollPosition;
+}
+
+// Listen for the scroll event on the window object
+window.addEventListener("scroll", toggleSecondNavbar);
+// Initialize the scrollContainer variable
+const scrollContainer = document.querySelector(".scroll-container");
+
+// Listen for the scroll event on the scroll container
+scrollContainer.addEventListener("scroll", toggleSecondNavbar);
+
+// ---------------- second navbar ends
+
+
+
+// ---------------- about section
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+  entries.forEach( entry => {
+    const el = entry.target
+    if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+      counterUp( el, {
+        duration: 2000,
+        delay: 16,
+      } )
+      el.classList.add( 'is-visible' )
     }
-  
-    // Listen for the scroll event and call the toggle function
-    window.addEventListener("scroll", toggleSecondNavbar);
-  });
+  } )
+}
+
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+const elements = document.querySelectorAll('.counter');
+elements.forEach((el) => {
+  IO.observe(el);
+});
+
+
+// ---------------- about section ends
+
+// ---------------- portfolio section
+const lerp = (f0, f1, t) => (1 - t) * f0 + t * f1;
+const clamp = (val, min, max) => Math.max(min, Math.min(val, max));
+
+class DragScroll {
+    constructor(obj) {
+        this.el = document.querySelector(obj.el);
+        this.wrap = document.querySelector("." + obj.wrap);
+        this.items = document.querySelectorAll(obj.item);
+        this.bar = document.querySelector(obj.bar);
+        this.init();
+    }
+
+    init() {
+        this.progress = 0;
+        this.speed = 0;
+        this.oldX = 0;
+        this.x = 0;
+        this.playrate = 0;
+
+        this.bindings();
+        this.events();
+        this.calculate();
+        this.raf();
+    }
+
+    bindings() {
+        [
+            "events",
+            "calculate",
+            "raf",
+            "move",
+            "handleTouchStart",
+            "handleTouchMove",
+            "handleTouchEnd",
+        ].forEach((method) => {
+            this[method] = this[method].bind(this);
+        });
+    }
+
+    calculate() {
+        this.progress = 0;
+        this.wrapWidth = this.items[0].clientWidth * this.items.length;
+        this.wrap.style.width = `${this.wrapWidth}px`;
+        this.maxScroll = this.wrapWidth - this.el.clientWidth;
+    }
+
+    handleTouchStart(e) {
+        e.preventDefault();
+        this.dragging = true;
+        this.startX = e.clientX || e.touches[0].clientX;
+    }
+
+    handleTouchMove(e) {
+        if (!this.dragging) return false;
+        const x = e.clientX || e.touches[0].clientX;
+        this.progress += (this.startX - x) * 2.5;
+        this.startX = x;
+        this.move();
+    }
+
+    handleTouchEnd() {
+        this.dragging = false;
+    }
+
+    move() {
+        this.progress = clamp(this.progress, 0, this.maxScroll);
+    }
+
+    events() {
+        window.addEventListener("resize", this.calculate);
+
+        this.el.addEventListener("touchstart", this.handleTouchStart);
+        window.addEventListener("touchmove", this.handleTouchMove);
+        window.addEventListener("touchend", this.handleTouchEnd);
+
+        window.addEventListener("mousedown", this.handleTouchStart);
+        window.addEventListener("mousemove", this.handleTouchMove);
+        window.addEventListener("mouseup", this.handleTouchEnd);
+        document.body.addEventListener("mouseleave", this.handleTouchEnd);
+    }
+
+    raf() {
+        this.x = lerp(this.x, this.progress, 0.1);
+        this.playrate = this.x / this.maxScroll;
+
+        this.wrap.style.transform = `translatex(${-this.x}px)`;
+        this.bar.style.transform = `scaleX(${0.18 + this.playrate * 0.82})`;
+
+        this.speed = Math.min(100, this.oldX - this.x);
+        this.oldX = this.x;
+
+        this.scale = lerp(this.scale, this.speed, 0.1);
+        this.items.forEach((item) => {
+            item.style.transform = `scale(${1 - Math.abs(this.speed) * 0.005})`;
+            item.querySelector("img").style.transform = `scaleX(${
+                1 + Math.abs(this.speed) * 0.004
+            })`;
+        });
+    }
+}
+
+const scroll = new DragScroll({
+    el: ".portfolio-slider",
+    wrap: "slider-wrapper-port",
+    item: ".slider-item-port",
+    bar: ".slider-progress-bar",
+});
+
+const animateScroll = () => {
+    requestAnimationFrame(animateScroll);
+    scroll.raf();
+}
+
+animateScroll();
+// ---------------- portfolio ends
+// ---------------- start testimonial
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 
 // ---------------- start services
 
@@ -146,83 +303,26 @@ $(function () {
     }
     accordion();
 
-    function cursor() {
-
-      $(window).on('mousemove', function (e) {
-
-        // cursor coord
-        let x = e.clientX;
-        let y = e.clientY;
-        
-        // cursot pos
-        $('.cursor').css({ left: x +'px', top: y + 'px' });
-      });
-
-      $('.accord-item').each(function() {
-
-        $(this).mouseenter(function () {
-            $('.cursor').addClass("active");
-        });
-
-        $(this).mouseleave(function () {
-            $('.cursor').removeClass("active");
-        });
-
-      })
-    }
-    cursor();
 })
 // ------------------ services ends
 
-// ------------------ clients start
 
-var copy = document.querySelector(".clients-slide").cloneNode(true);
-document.querySelector('.clients').appendChild(copy);
 
-//  ------------------ clients start ends
 
-// ------------------- achivements starts
+// ------------------ scroll to top starts
 
-    let nums = document.querySelectorAll(".content .num");
-    let div = document.querySelector(".achievements");
-    let started = false;
+const span = document.querySelector(".up");
 
-    nums.forEach((num) => startCount(num));
+scrollContainer.addEventListener("scroll", function () {
+  this.scrollTop >= 1000
+    ? span.classList.add("show")
+    : span.classList.remove("show");
+});
 
-    // window.onscroll = function () {
-    //     if (window.scrollY >= div.offsetTop) {
-    //         if (!started) {
-    //             nums.forEach((num) => startCount(num));
-    //         }
-    //         started = true;
-    //     }
-    // }
-
-    function startCount(el) {
-        let goal = el.dataset.goal;
-        let count = setInterval(() => {
-            el.textContent++;
-            if (el.textContent == goal) {
-                clearInterval(count);
-            }
-        }, 2000 / goal);
-    }
-
-// ------------------- achivements ends
-
-// ------------------- scroll to top starts
-
-let span = document.querySelector(".up");
-
-window.onscroll = function () {
-    this.scrollY >= 1000 ? span.classList.add("show") : span.classList.remove("show");
-};
-
-span.onclick = function () {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    })
-}
-
+span.addEventListener("click", function () {
+  scrollContainer.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
